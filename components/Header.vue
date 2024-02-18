0cce92db46e3b14719b28ea885ebaa845a15c470
Younes Barrad <younesbarraddev@gmail.com>
2024-02-18 13:09:18 +0100

    fix: updated class for github stars

--- components/Header.vue
+++ components/Header.vue
@@ -36,7 +36,7 @@ const { header } = useAppConfig()
           to="https://github.com/vue-email/vue-email"
           target="_blank"
           :label="stats && stats.stats && stats.stats.stars ? formatNumber(stats.stats.stars) : '...'"
-          class="hidden md:block"
+          class="hidden md:flex"
           v-bind="$ui.button.secondary as any"
         />
       </UTooltip>
