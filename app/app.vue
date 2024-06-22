5117f88eb91b858e5841f9e9b451d75f49cd96ad
Younes Barrad <younesbarraddev@gmail.com>
2024-06-23 00:40:54 +0100

    chore: updated og images

--- app/app.vue
+++ app/app.vue
@@ -24,8 +24,8 @@ useHead({
 useSeoMeta({
   titleTemplate: `%s - ${seo?.siteName}`,
   ogSiteName: seo?.siteName,
-  ogImage: 'https://docs-template.nuxt.dev/social-card.png',
-  twitterImage: 'https://docs-template.nuxt.dev/social-card.png',
+  ogImage: 'https://vuemail.net/social-preview.jpg',
+  twitterImage: 'https://vuemail.net/social-preview.jpg',
   twitterCard: 'summary_large_image'
 })
 
